da528ddcb63345cff67a6ac2d00e74ab44b98089
Oriantar <77988256+Oriantar@users.noreply.github.com>
2023-05-19 00:05:31 +0200

    Updated

--- ESP32/RadioCode/vs1053_ext/hoi/vs_1053_ext/vs_1053_ext.ino
+++ ESP32/RadioCode/vs1053_ext/hoi/vs_1053_ext/vs_1053_ext.ino
@@ -5,122 +5,35 @@
 //#include "FS.h"
 #include "vs1053_ext.h"
 
-#include <WiFi.h>
-#include <WiFiMulti.h>
-
-#include <HTTPClient.h>
-
-
 // Digital I/O used
-// #define SD_CS        5
+//#define SD_CS        5
 #define VS1053_MOSI   23
 #define VS1053_MISO   19 
 #define VS1053_SCK    18
 #define VS1053_CS      2
 #define VS1053_DCS     4
 #define VS1053_DREQ   35
-#define USE_SERIAL Serial
-
-WiFiMulti wifiMulti;
-long start = 0;
-long requestInterval = 1000;
 
 String ssid =     "Glamdring";
 String password = "12345678";
-String zender = "https://i2.cdn.jetstre.am:8000/sz=efteling=lqstream?token=b74a66251c3760948f7c59a6932efead&time=63ecbb63"; 
 
-int volume=10;
+int volume=15;
 
 VS1053 mp3(VS1053_CS, VS1053_DCS, VS1053_DREQ, VSPI, VS1053_MOSI, VS1053_MISO, VS1053_SCK);
 
 //The setup function is called once at startup of the sketch
-String checkZender(){
-  
-  if((wifiMulti.run() == WL_CONNECTED)) {
-
-    HTTPClient http;
-
-    USE_SERIAL.print("[HTTP] begin...\n");
-        // configure traged server and url
-        //http.begin("https://www.howsmyssl.com/a/check", ca); //HTTPS
-    http.begin("http://pi.local/radioOutput"); //HTTP
-
-    USE_SERIAL.print("[HTTP] GET...\n");
-        // start connection and send HTTP header
-    int httpCode = http.GET();
-
-        // httpCode will be negative on error
-    if(httpCode > 0) {
-            // HTTP header has been send and Server response header has been handled
-    USE_SERIAL.printf("[HTTP] GET... code: %d\n", httpCode);
-
-            
-    if(httpCode == HTTP_CODE_OK) {
-      String payload = http.getString();
-                USE_SERIAL.println(payload);
-                
-                zender = String(payload);
-                return zender;
-            }
-        } else {
-            USE_SERIAL.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
-        }
-
-        http.end();
-    }
-
-}
-
-void checkVolume(){
-      
-  if((wifiMulti.run() == WL_CONNECTED)) {
-
-    HTTPClient http;
-
-    USE_SERIAL.print("[HTTP] begin...\n");
-        // configure traged server and url
-        //http.begin("https://www.howsmyssl.com/a/check", ca); //HTTPS
-    http.begin("http://pi.local/volumeOutput"); //HTTP
-
-    USE_SERIAL.print("[HTTP] GET...\n");
-        // start connection and send HTTP header
-    int httpCode = http.GET();
-
-        // httpCode will be negative on error
-    if(httpCode > 0) {
-            // HTTP header has been send and Server response header has been handled
-    USE_SERIAL.printf("[HTTP] GET... code: %d\n", httpCode);
-
-            
-    if(httpCode == HTTP_CODE_OK) {
-      String payload = http.getString();
-                USE_SERIAL.println(payload);
-                volume = payload.toInt();
-                
-                
-            }
-        } else {
-            USE_SERIAL.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
-        }
-
-        http.end();
-    }
-}
 void setup() {
     //pinMode(SD_CS, OUTPUT);      digitalWrite(SD_CS, HIGH);
     Serial.begin(115200);
     SPI.begin(VS1053_SCK, VS1053_MISO, VS1053_MOSI);
-    wifiMulti.addAP("Glamdring", "12345678");
     //SD.begin(SD_CS);
     WiFi.disconnect();
     WiFi.mode(WIFI_STA);
     WiFi.begin(ssid.c_str(), password.c_str());
     while (WiFi.status() != WL_CONNECTED) delay(1500);
-    
-    // checkVolume();
-    
-    MP3Unit();
-    
+    mp3.begin();
+    mp3.setVolume(volume);
+    mp3.connecttohost("streambbr.ir-media-tec.com/berlin/mp3-128/vtuner_web_mp3/");
     //mp3.connecttohost("stream.landeswelle.de/lwt/mp3-192");                 // mp3 192kb/s
     //mp3.connecttohost("http://radio.hear.fi:8000/hear.ogg");                // ogg
     //mp3.connecttohost("tophits.radiomonster.fm/320.mp3");                   // bitrate 320k
@@ -131,32 +44,20 @@ void setup() {
     //mp3.connecttohost("http://stream.revma.ihrhls.com/zc5060/hls.m3u8");    // HLS
     //mp3.connecttohost("https://mirchiplaylive.akamaized.net/hls/live/2036929/MUM/MEETHI_Auto.m3u8"); // HLS transport stream
     //mp3.connecttoFS(SD, "320k_test.mp3"); // SD card, local file
-    
+    //mp3.connecttospeech("Wenn die Hunde schlafen, kann der Wolf gut Schafe stehlen.", "de");
 }
 
 // The loop function is called in an endless loop
 void loop()
-{   
+{
     mp3.loop();
 }
 
-void MP3Unit(){
-    mp3.begin();
-    mp3.setVolume(volume);
-    mp3.connecttohost(zender);
-    
-}
-
-
-
-
-
-
 // next code is optional:
-// void vs1053_info(const char *info) {                // called from vs1053
-//     Serial.print("DEBUG:        ");
-//     Serial.println(info);                           // debug infos
-// }
+void vs1053_info(const char *info) {                // called from vs1053
+    Serial.print("DEBUG:        ");
+    Serial.println(info);                           // debug infos
+}
 void vs1053_showstation(const char *info){          // called from vs1053
     Serial.print("STATION:      ");
     Serial.println(info);                           // Show station name
